b63c09ec811418996626472976c1d5d6e1ca1799
Mark Odell <m4rk.odell@gmail.com>
2019-05-12 14:42:29 -0700

    Reformat setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# TODO Everything here... 
+
 function readYes() {
   until [[ $REPLY == "Y" || $REPLY == "N" ]]; do
     printf "%s [Y,N]: " "$1"
@@ -15,15 +17,15 @@ function readYes() {
 
 readYes "Would you like to install homebrew?" HOMEBREW
 if ( $ENABLE_HOMEBREW ); then 
-echo "Installing homebrew..."
-#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  echo "Installing homebrew..."
+  #/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-echo "Listing everything from the Brewfile..."
-brew bundle list --all
+  echo "Listing everything from the Brewfile..."
+  brew bundle list --all
 
-readYes "Would you like to use 'brew bundle' to install the above?" HOMEBREW_BUNDLE 
-if ( $ENABLE_HOMEBREW_BUNDLE ); then
-brew bundle --file=./Brewfile
+  readYes "Would you like to use 'brew bundle' to install the above?" HOMEBREW_BUNDLE 
+  if ( $ENABLE_HOMEBREW_BUNDLE ); then
+  brew bundle --file=./Brewfile
+  fi
 fi
 
-fi
